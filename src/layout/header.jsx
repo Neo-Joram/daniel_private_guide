cc4e29162bb0b401e9b34e76c4f4bea2b4b17db9
neo joram <neojoram12@gmail.com>
2024-03-11 13:47:54 +0200

    bg

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Profile, Career, Navmenu } from ".";
+import giraffe from "../assets/animals/giraffe_3.jpg";
 // import lion from "../assets/animals/lion_3.jpg";
 
 export default function Header() {
   return (
-    <div className={`relative pb-14 `}>
+    <div className={`relative pb-14 overflow-hidden`}>
       <Navmenu />
       <Profile />
       <Career />
+      <img
+        src={giraffe}
+        alt="giraffe"
+        className="absolute top-0 right-0 -z-50"
+      />
     </div>
   );
 }
